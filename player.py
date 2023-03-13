9321a828828f64a528ec138890745aac37672f77
REKAR AXEL <axkeldeo@gmail.com>
2023-03-13 13:26:20 +0100

    Start

---
+++ player.py
@@ -0,0 +1,147 @@
+import random
+from abc import ABC, abstractmethod
+import math
+from anytree import Node
+
+class Player(ABC) :
+    def __init__(self,name) :
+        self.name = name
+        self.number = 0
+    
+    @abstractmethod
+    def moveChoice(self,board,clickedColumn = None):
+        pass
+    
+
+class RandomPlayer(Player) :
+
+    def moveChoice(self,board) : 
+        return random.randint(1,board.cols)
+
+
+class ConsolePlayer(Player) :
+
+    def moveChoice(self,board) : 
+        while(True) :
+                column = input("Play in which column ? : ").strip()
+                if column.isdigit() :
+                    return int(column)
+                else : 
+                    print("invalid entry")
+
+class InterfacePlayer(Player) :
+    def moveChoice(self,board,clickedColumn) : 
+        return clickedColumn
+
+class MinMaxPlayer(Player) :
+
+    def __init__(self, name, depth = 7):
+        super().__init__(name)
+        self.depth = depth
+
+    def moveChoice(self,board) :  
+        if board.winNextMove() >= 0 :
+            return board.winNextMove()
+        
+        values = [-1000] * board.cols
+
+        for i in range(board.cols) :
+            if board.firstfreerow[i] < board.rows :
+                nb = board.clone()
+                values[i] = self.minmax(nb.play(i),self.depth-1,-math.inf,math.inf,False) 
+        
+        return values.index(max(values)) + 1
+    
+    def minmax(self,board, depth : int, alpha : int, beta : int, maximizing_player : bool) -> int :
+         return random.randint(1,7)
+
+class MCSTPlayer(Player) :
+
+    def __init__(self, name, iter = 1000):
+        super().__init__(name)
+        self.iter = iter
+
+    def moveChoice(self,board) : 
+        newboard = board.clone()
+        values = self.MCST(newboard,1.41)
+        return values.index(max(values)) + 1
+
+    def UCB1(self,played : int, won : int,N : int,c : float) -> float :
+        if played == 0 : return math.inf
+        return won / played + c * math.sqrt(math.log(N)/played)
+    
+    def MCST(self,board,c : float) -> list[int] :
+
+        root = Node("0",board=board,won=0,played=1)
+        denom = [0] * board.cols
+
+        for i in range(board.cols) :
+            if(board.firstfreerow[i] < board.rows) :
+                nb = board.clone()
+                nb.play(i)
+                Node("0"+ str(i),board=nb,won=0,played=0,parent=root)
+            else :
+                denom[i] = -1
+
+        iteration = self.iter
+
+        while(iteration > 0) :
+
+            currentNode : Node = root
+
+            #finding a leaf node
+            while (len(currentNode.children) > 0) :
+                succUCB = []
+                for succ in currentNode.children :
+                    succUCB.append(self.UCB1(played=succ.played,won=succ.won,N=currentNode.played,c=c))
+                
+                currentNode = currentNode.children[succUCB.index(max(succUCB))]
+            
+            #creating children
+            for i in range(board.cols) :
+                if(board.firstfreerow[i] < board.rows) :
+                    nb = board.clone()
+                    nb.play(i)
+                    Node(currentNode.name + str(i),board=nb,won=0,played=0,parent=currentNode)
+                
+            #selecting a random son
+            selectedson = currentNode.children[random.randint(0,len(currentNode.children) - 1)]
+
+            #random playout
+            start = selectedson.board.clone()
+            while start.checkWin() < 0 and start.turnplayed < start.rows * start.cols :
+                start.play(random.randint(1,start.cols))
+            
+            won = True if self.name != start.player.name else False
+
+            #backpropagate
+            while selectedson.parent != root :
+                selectedson.played += 1
+                if won and selectedson.board.player.name != self.name : selectedson.won += 1
+                if not won and selectedson.board.player.name == self.name : selectedson.won += 1
+                selectedson = selectedson.parent
+                print(selectedson.name)
+            
+            root.played += 1
+
+            iteration -= 1
+
+        #prepare denom array
+        count = 0
+        for i in range(board.cols) :
+            if denom[i] >= 0 :
+                denom[i] == root.children[count].played
+                count += 1
+
+        return denom
+
+    
+
+if __name__ == '__main__' :
+    root = Node("A")
+
+    # Create some child nodes and add them to the root node
+    b_node = Node("B", parent=root)
+    c_node = Node("C", parent=root)
+
+    print(len(root.children))
---
+++ proje.py
@@ -0,0 +1,159 @@
+import copy
+from player import Player,RandomPlayer,MinMaxPlayer,MCSTPlayer
+import time
+
+class Board :
+    def __init__(self,player1 : Player,player2: Player,rows = 6,cols = 7):
+        self.grille = []
+        self.rows = rows if rows > 3 else 4
+        self.cols = cols if cols > 3 else 4
+        self.firstfreerow = [0] * self.cols
+        for i in range(self.rows):
+            self.grille.append([-1] * self.cols)
+        self.lastplay = (0,0)
+        self.turnplayed = 0
+        player1.number = 0
+        player2.number = 1
+        self.player1 = player1
+        self.player2 = player2
+        self.player = player1
+    
+    def getOtherPlayerName(self) :
+        return self.player1.name if self.player == self.player2 else self.player2.name
+
+    #console print of board with chosen symbols 
+    def print(self,p0symbol = "O",p1symbol = "X") :
+        board = ""
+        for i in range(self.rows - 1,-1,-1) :
+            for j in range(self.cols) : 
+                board += "|"
+                if self.grille[i][j] == 0 :
+                    board += p0symbol
+                elif self.grille[i][j] == 1 :
+                    board += p1symbol
+                else : 
+                    board += " "
+            board += "|\n"
+        print(board)
+    
+    #clone a board with deepcopy function
+    def clone(self): 
+        return copy.deepcopy(self)
+
+    #play a move in current game board
+    def play(self,column) -> bool : 
+        truecol = column - 1
+        if column > self.cols or self.firstfreerow[truecol] >= self.rows :
+            return False
+        else :
+            self.grille[self.firstfreerow[truecol]][truecol] = self.player.number
+            self.lastplay = (self.firstfreerow[truecol],truecol)
+            self.firstfreerow[truecol] +=1 
+            self.player = self.player1 if self.player.number == 1 else self.player2
+            self.turnplayed += 1
+            return True
+
+    #if win, return player number else -1
+    def checkWin(self) -> int : 
+
+        if self.turnplayed ==  0  : return -1
+    
+        row, col = self.lastplay
+        symbol = self.grille[row][col]
+
+        #check vertical
+        if row >= 3 : 
+            if self.grille[row-1][col] == symbol and self.grille[row-2][col] == symbol and self.grille[row-3][col] == symbol : return symbol
+
+        #check right and left
+        sameright,sameleft = True,True
+        countaligned = 1
+        positionright,positionleft= col,col
+
+        while sameright or sameleft :
+            if sameright and positionright + 1 < self.cols and self.grille[row][positionright + 1] == symbol :
+                positionright += 1
+                countaligned += 1
+            else : 
+                sameright = False
+            
+            if sameleft and positionleft - 1 >= 0 and self.grille[row][positionleft - 1] == symbol :
+                positionleft -= 1
+                countaligned += 1
+            else : 
+                sameleft = False
+        
+        if countaligned > 3 : return symbol
+
+        #left-up to right-bottom diagonal
+        sameright,sameleft = True,True
+        countaligned = 1
+        p1row,p1col= row,col
+        p2row,p2col = row,col
+
+        while sameright or sameleft :
+            p1row -= 1
+            p1col += 1
+            if sameright and p1row >= 0 and p1col < self.cols and self.grille[p1row][p1col] == symbol :
+                countaligned += 1
+            else : 
+                sameright = False
+
+            p2row +=1
+            p2col -= 1
+
+            if sameleft and p2row < self.rows and p2col >= 0 and self.grille[p2row][p2col] == symbol :
+                countaligned += 1
+            else :
+                sameleft = False
+
+        if countaligned > 3 : return symbol
+
+        #left-bottom to right up diagonal
+        sameright,sameleft = True,True
+        countaligned = 1
+        p1row,p1col= row,col
+        p2row,p2col = row,col
+
+        while sameright or sameleft :
+            p1row += 1
+            p1col += 1
+            if sameright and p1row < self.rows and p1col < self.cols and self.grille[p1row][p1col] == symbol :
+                countaligned += 1
+            else : 
+                sameright = False
+
+            p2row -=1
+            p2col -= 1
+
+            if sameleft and p2row >= 0 and p2col >= 0 and self.grille[p2row][p2col] == symbol :
+                countaligned += 1
+            else :
+                sameleft = False
+
+        if countaligned > 3 : return symbol
+        return -1
+    
+    #check if immediate win is possible
+    def winNextMove(self) :
+        for i in range(self.cols) :
+            nb = self.clone()
+            nb.play(i+1)
+            if nb.checkWin() >= 0:
+                return i+1       
+        return -1
+
+if __name__ == '__main__': 
+    board = Board(RandomPlayer("Axel"),MCSTPlayer("Lexa"))
+
+    while board.checkWin() < 0 and board.turnplayed < board.rows * board.cols :
+
+        if board.play(board.player.moveChoice(board))  : #try to play, print if move successful
+            print(board.getOtherPlayerName())
+            board.print()
+            time.sleep(1)
+            
+if board.turnplayed < board.rows * board.cols :
+    print("Win : " + board.getOtherPlayerName())
+else :
+    print("Draw")
